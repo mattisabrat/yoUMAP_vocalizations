3fb813cd1fdc223d662307d10d24c4e7e0ea8419
Matthew Davenport <mdavenport@v-virtual15.rockefeller.edu>
2019-12-16 15:27:57 -0500

    Works.

--- .Pipe_Modules/cluster_syllables.py
+++ .Pipe_Modules/cluster_syllables.py
@@ -32,15 +32,12 @@ from avgn.network.training import load_from_hdf5
 opts, args = getopt.getopt(sys.argv[1:], 'i:o:n:s:c:m:p:')
 
 for opt, arg in opts:
-    if   opt in ('-i'): hdf_list      = str(arg)
+    if   opt in ('-i'): hdf_loc       = str(arg)
     elif opt in ('-o'): syllable_path = str(arg)
     elif opt in ('-n'): nThreads      = int(arg)
     elif opt in ('-s'): animal        = str(arg)
     elif opt in ('-c'): config_path   = str(arg)
     elif opt in ('-m'): mode          = str(arg)
-
-#Split the input list into a vecotr we can iterate on
-hdf_list = hdf_list.split('__SPLIT__')
 
 
 ##Read in params
@@ -62,41 +59,41 @@ seq_len_cutoff = int(  config.get('sequence_syllables', 'seq_len_cutoff'))
 #define the hdf fields
 to_load     = ['spectrograms', 'lengths', 'start', 'wav_file', 'syll_start_rel_wav']
 
-#load the hdfs into a list then convert to dataframe
-hdf_content_list = [load_from_hdf5([hdf_loc], to_load) for hdf_loc in hdf_list]
-hdf_content_list = [pd.DataFrame({
-        'spectrograms':       contents['spectrograms'].tolist(), 
-        'syllable_time':     [datetime.strptime(i[0], '%d/%m/%y %H:%M:%S.%f') for i in contents['start'].astype('str').tolist()] , 
-        'syll_length_s':      contents['lengths'].tolist(), 
-        'start_time_rel_wav': contents['syll_start_rel_wav'].tolist(), 
-        'original_wav':       contents['wav_file'].tolist(), 
-        'animal':             contents['name'].tolist()    
-    }) for contents in hdf_content_list]
+#load the hdfs 
+hdf_content = load_from_hdf5([hdf_loc], to_load)
 
-#merge the conent of the dataframes in the list
-#handles the experiment level clustering
-hdf_content = pd.concat(hdf_content_list)
 #Handle the not enough syllables case
-if ( len(hdf_content['animal']) < min_syls):
+if ( len(hdf_content['name']) < min_syls):
     logging.error('%s has %s syllables,  min is %s , will not cluster.' %
                   (animal, len(hdf_content['animal']), min_syls,))
     sys.exit(1)
+
 # embed spectrograms in low dim space
 #resize
-x = np.asarray(hdf_content['spectrograms'])
-x_small = [resize(np.asarray(i), [16,16]) for i in x]
+x = hdf_content['spectrograms']
+x_small = [resize(i, [16,16]) for i in x]
 x_small = np.array(x_small).reshape((len(x_small), np.prod(np.shape(x_small)[1:])))
 x_small = [(i*255).astype('uint8') for i in x_small]
 
 #embed
-hdf_content['z'] = umap.UMAP(
+clusterable_embedding = umap.UMAP(
     n_neighbors  = n_neighbors,
     min_dist     = min_dist,
     n_components = n_components,
     random_state = random_state,
-).fit_transform(x_small).tolist()
+).fit_transform(x_small)
 
-#Convert the spectrograms to a list for consistency
+#Convert to pd.df
+hdf_content = pd.DataFrame({
+        'spectrograms':       hdf_content['spectrograms'].tolist(), 
+        'z':                  clusterable_embedding.tolist(),
+        'syllable_time':     [datetime.strptime(i[0], '%d/%m/%y %H:%M:%S.%f') for i in hdf_content['start'].astype('str').tolist()] , 
+        'syll_length_s':      hdf_content['lengths'].tolist(), 
+        'start_time_rel_wav': hdf_content['syll_start_rel_wav'].tolist(), 
+        'original_wav':       hdf_content['wav_file'].tolist(), 
+        'animal':             hdf_content['name'].tolist()    
+    }) 
+
 
 #define min num of syls per cluster
 min_cluster_size = int(len(hdf_content['animal'])*cluster_pct)
@@ -113,7 +110,7 @@ frac_unlabelled = np.sum(hdf_content['labels'] == -1)/len(hdf_content['labels'])
 logging.info('%s had %s/1 of syllables unlabelled' % (animal, frac_unlabelled,))
     
 #Add sequence info to the dataframe
-content = split_times_into_seqs(hdf_content, max_timedelta, seq_len_cutoff)
+hdf_content = split_times_into_seqs(hdf_content, max_timedelta, seq_len_cutoff)
 
 #save the dataframe
 hdf_content.to_csv(syllable_path)
